7b4caf589cc05898118fa880e9f9f85f02482fc0
Andriikryl <andreyk557@gmail.com>
2023-04-03 20:10:21 +0300

    feat[random-number]

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-const number = document.querySelector(".js-");
+const number = document.querySelector(".js-number");
 const parameters = document.querySelector(".js-parameters");
 const rangeInput = document.querySelector(".js-range-input");
 const minNumber = document.querySelector(".js-min-number");
@@ -19,4 +19,16 @@ listNumbers.addEventListener("input", (e) => {
   e.target.value = e.target.value.replace(/[^\s\d]/g, "");
 });
 
-listNumbers.addEventListener("click", (e) => {});
+btn.addEventListener("click", (e) => {
+  if (rangeInput.checked) {
+    number.innerHTML = Math.floor(
+      Number(minNumber.value) +
+        Math.random() * (Number(maxNumber.value) + 1 - Number(minNumber.value))
+    );
+  }
+  if (listInput.checked) {
+    let arrListNumbers = listNumbers.value.trim().split(/\s+/);
+    number.innerHTML =
+      arrListNumbers[Math.floor(Math.random() * arrListNumbers.length)];
+  }
+});
